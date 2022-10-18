c858b988a3378ca3ae8db71c9187623e2130412b
aivvas <vassi.aiva@gmail.com>
2022-10-18 10:41:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,6 +6,13 @@ const cat = [
     console.log(complain)
 ];
 
-//Question 2
+//Question 2, question 3
+var h3 = document.querySelector("h3");
+var heading = document.querySelector("heading");
 
+function h3heading(){
+    h3.innerHTML = "Updated heading";
+    h3.style.fontSize = "2em";
+};
+h3heading();
 
