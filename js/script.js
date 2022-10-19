d13d66e869e9cfbab7af07e899677216bb13eed0
aivvas <vassi.aiva@gmail.com>
2022-10-19 11:54:50 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -5,33 +5,36 @@ const cat = {
 
 console.log(cat["complain"]);
 
-//Question 2, question 3
+//Question 2, question 3, Question 4
 var h3 = document.querySelector("h3");
 var heading = document.querySelector("heading");
 
 function h3heading(){
     h3.innerHTML = "Updated heading";
     h3.style.fontSize = "2em";
+    h3.classList.add("subheading");
 };
 h3heading();
 
-//Question 4
-function addclass(){
-    var heading = document.querySelector("heading");
-    heading.classlist.add("subheading");
-}
-addclass();
-
 //Question 5
+var paragraphs = document.querySelector("p");
+for (var i=0, max=paragraphs.clientHeight; i < max; i++){
+    paragraphs.style.backgroundColor = "red";
+};
+
 
 //Question 6
-var resultsContainer = document.querySelector("results");
+var resultsContainer = document.querySelector(".results");
 function resultsC(){
     resultsContainer.innerHTML = "<p>New paragraph</p>";
     resultsContainer.style.backgroundColor = "yellow";
 };
 resultsC();
+
 //Question 7
+function listpar(list){
+
+}
 
 //Question 8
 const cats = [
