a736e947f1c10417d5aca98ce7f15793ccdf2e17
aivvas <vassi.aiva@gmail.com>
2022-10-19 10:30:59 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,9 @@
 //Question 1
-const cat = [
-    {
-        complain: "meow!"
-    },
-    console.log(complain)
-];
+const cat = {
+    complain: "meow!"
+};
+
+console.log(cat["complain"]);
 
 //Question 2, question 3
 var h3 = document.querySelector("h3");
@@ -15,4 +14,29 @@ function h3heading(){
     h3.style.fontSize = "2em";
 };
 h3heading();
+
+//Question 4
+
+//Question 5
+
+//Question 6
+
+//Question 7
+
+//Question 8
+const cats = [
+    {
+        name: "Blob",
+        age: 10
+    },
+    {
+        name: "Harold",
+    },
+    {
+        name: "Blurt",
+        age: 21
+    }
+];
+
+
 
